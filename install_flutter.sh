ea9a53aef61b984136e05740f64132ec3fc798d5
Backershan <backershan@yachii.com>
2025-08-22 19:15:10 +0530

    up

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-# Install Flutter SDK
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
+# Install Flutter only if not already present
+if [ ! -d "flutter" ]; then
+  echo "Cloning Flutter SDK..."
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+else
+  echo "Flutter SDK already exists, skipping clone."
+fi
+
+# Add Flutter to PATH
 export PATH="$PATH:`pwd`/flutter/bin"
 
 # Enable web support & pre-cache
